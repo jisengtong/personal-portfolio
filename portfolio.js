bee370ba5e1c952bcc2afd66d034eb34b726f5b8
Tong Ji Seng <81406964+jisengtong@users.noreply.github.com>
2022-05-20 11:27:31 +0800

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -2,7 +2,7 @@
 let quotes = ['The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela',
     `If you look at what you have in life, you'll always have more. -Oprah Winfrey`,
     `Life is what happens when you're busy making other plans. -John Lennon`, `When you reach the end of your rope, tie a knot in it and hang on. -Franklin D. Roosevelt`,
-    `Whoever is happy will make others happy too. -Anne Frank`, `A good day starts with a cup of coffee. -Myself`];
+    `Whoever is happy will make others happy too. -Anne Frank`, `A good day starts with a cup of coffee.`];
 let randomQuotes = quotes[Math.floor(Math.random() * quotes.length)];
 
 //responsive nav button toggler
